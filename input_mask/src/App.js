6f23e1b23edb0d51843101f598a9d294bb1debdd
Ali Akbar Shahi <aliakbarshahice@gmail.com>
2022-01-21 20:30:47 +0330

    done with score 100

--- input_mask/src/App.js
+++ input_mask/src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import Input from "./Input";
+import Cities from "./cities.json";
 
 function App() {
-   return <div></div>
+  const [hint, setHint] = useState("");
+
+  const onInputChange = (value) => {
+    if (value == "") {
+      setHint("");
+      return;
+    }
+    const mathed = Cities.filter((i) => i.startsWith(value));
+    if (mathed.length > 0) {
+      setHint(mathed[0]);
+    } else setHint("");
+  };
+  return (
+    <div>
+      <Input
+        handleChange={(e) => {
+          onInputChange(e.target.value);
+        }}
+        hint={hint}
+      />
+    </div>
+  );
 }
 
 export default App;
